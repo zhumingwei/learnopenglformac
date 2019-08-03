f63c8db6b11c81d62fdce24632f1939f832d0c4c
zhumingwei <zhumingwei2016@gmail.com>
2019-08-03 12:08:12 +0800

    着色器

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # learnopenglformac
 ##代码记得修改tasks.json和launch.json路径配置
-1，源码安装glfw
+1，源码安装glfw （动态库 cmake -D BUILD_SHARED_LIBS=ON ..）
 2，安装glad
--- opengl/hellow.cpp
+++ opengl/hellow.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
+#include "shader_s.h"
 
+#include <unistd.h>
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 // settings
 const unsigned int SCR_WIDTH = 1000;
 const unsigned int SCR_HEIGHT = 800;
 
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
-
 int main(){
+    const int MAXPATH = 250;
+    char buffer[MAXPATH];
+    getcwd(buffer, MAXPATH);
+    std::cout << "The current directory is:" << buffer << std::endl;
+    printf("The current directory is: %s", buffer);
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -48,42 +43,9 @@ int main(){
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
-    }    
-    //vertex shader
-    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
-    glCompileShader(vertexShader);
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader,GL_COMPILE_STATUS,&success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader,512,NULL,infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
-    //fragment shader
-    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader,1,&fragmentShaderSource,NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader,GL_COMPILE_STATUS,&success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader,512,NULL,infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram,fragmentShader);
-    glLinkProgram(shaderProgram);
-    // check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-    
+    Shader ourShader("opengl/shaders/shader.vs", "opengl/shaders/shader.fs");
+
     float vertices[] = {
         0.5f, 0.5f, 0.0f,   // 右上角
         0.5f, -0.5f, 0.0f,  // 右下角
@@ -128,8 +90,8 @@ int main(){
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        ourShader.use();
         //draw triangle
-        glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         // glDrawArrays(GL_TRIANGLES,0,3);
         // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);

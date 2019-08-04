7d87e09a90c5c3e460ed2fc098ce90860cffa297
zhumingwei <zhumingwei2016@gmail.com>
2019-08-04 19:16:53 +0800

    坐标系统4

--- opengl/hellow.cpp
+++ opengl/hellow.cpp
@@ -155,6 +155,19 @@ int main(){
     ourShader.use();
     ourShader.setInt("texture1", 0);
     ourShader.setInt("texture2", 1);
+
+    glm::vec3 cubePositions[] = {
+  glm::vec3( 0.0f,  0.0f,  0.0f), 
+  glm::vec3( 2.0f,  5.0f, -15.0f), 
+  glm::vec3(-1.5f, -2.2f, -2.5f),  
+  glm::vec3(-3.8f, -2.0f, -12.3f),  
+  glm::vec3( 2.4f, -0.4f, -3.5f),  
+  glm::vec3(-1.7f,  3.0f, -7.5f),  
+  glm::vec3( 1.3f, -2.0f, -2.5f),  
+  glm::vec3( 1.5f,  2.0f, -2.5f), 
+  glm::vec3( 1.5f,  0.2f, -1.5f), 
+  glm::vec3(-1.3f,  1.0f, -1.5f)  
+};
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -178,21 +191,28 @@ int main(){
        ourShader.use();
 
        //create transformations
-       glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+       
        glm::mat4 view = glm::mat4(1.0f);
        glm::mat4 projection = glm::mat4(1.0f);
-       model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 1.0f));
+       
        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
-       ourShader.setMat4("model", model);
+       
        ourShader.setMat4("view", view);
        // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
        ourShader.setMat4("projection", projection);
 
-       glBindVertexArray(VAO);
-       glDrawArrays(GL_TRIANGLES, 0, 36);
-
+       for (unsigned int i = 0; i < 10; i++)
+       {
+           glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+           model = glm::translate(model, cubePositions[i]);
+           model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 1.0f));
+           glBindVertexArray(VAO);
+           ourShader.setMat4("model", model);
+           glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+        
        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
        // -------------------------------------------------------------------------------
        glfwSwapBuffers(window);

550ad3c4e38338893628b5f0e14be74eb9760db8
zhumingwei <zdf312192599@163.com>
2019-08-06 10:39:01 +0800

    投光物(聚光)

--- opengl/hellow.cpp
+++ opengl/hellow.cpp
@@ -236,7 +236,7 @@ int main(){
        {
            glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
            model = glm::translate(model, cubePositions[i]);
-           model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 1.0f));
+           model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
            glBindVertexArray(VAO);
            ourShader.setMat4("model", model);
            glDrawArrays(GL_TRIANGLES, 0, 36);

694a80886bec0d13155bf1c8070d6dc244675cdb
zhumingwei <zdf312192599@163.com>
2019-08-06 19:53:05 +0800

    混合

--- opengl/hellow.cpp
+++ opengl/hellow.cpp
@@ -11,6 +11,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <map>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -70,11 +71,12 @@ int main(){
         return -1;
     }
     Shader ourShader("opengl/shaders/shader.vs", "opengl/shaders/shader.fs");
-    Shader lampShader("opengl/shaders/lamp.vs", "opengl/shaders/lamp.fs");
+    Shader shader2("opengl/shaders/lamp.vs", "opengl/shaders/lamp.fs");
 
     glEnable(GL_DEPTH_TEST);
-    glEnable(GL_STENCIL_TEST);//模板缓冲
-
+    // glEnable(GL_STENCIL_TEST);//模板缓冲
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
@@ -160,37 +162,79 @@ int main(){
     glBindVertexArray(lightVAO);
     glBindBuffer(GL_ARRAY_BUFFER,VBO);
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
-        //绑定position (0)
+    //绑定position (0)
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,8 * sizeof(float),(void*)0);
     glEnableVertexAttribArray(0);
+
+    float transparentVertices[] = {
+        // positions         // texture Coords (swapped y coordinates because texture is flipped upside down)
+        0.0f,  0.5f,  0.0f,  0.0f,  0.0f,
+        0.0f, -0.5f,  0.0f,  0.0f,  1.0f,
+        1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
+
+        0.0f,  0.5f,  0.0f,  0.0f,  0.0f,
+        1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
+        1.0f,  0.5f,  0.0f,  1.0f,  0.0f
+    };
+
+    // transparent VAO
+    unsigned int transparentVAO, transparentVBO;
+    glGenVertexArrays(1, &transparentVAO);
+    glGenBuffers(1, &transparentVBO);
+    glBindVertexArray(transparentVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, transparentVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(transparentVertices), transparentVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glBindVertexArray(0);
    
 
     // load and create a texture
     // -------------------------
 
     // load image, create texture and generate mipmaps
-    int width, height, nrChannels;
-    unsigned char *data;
     texture1 = genTexture("opengl/textures/container2.png");
     texture2 = genTexture("opengl/textures/container2_specular.png");
+    unsigned inttexture_kuang = genTexture("opengl/textures/blending_transparent_window.png");
 
     ourShader.use();
     //配置textureID 后面需要绑定根据这个数字， GL_TEXTUREX
     ourShader.setInt("material.diffuse",0);
     ourShader.setInt("material.specular",1);
-
+    shader2.setInt("texture1",0);
     glm::vec3 cubePositions[] = {
-  glm::vec3( 0.0f,  0.0f,  0.0f), 
-  glm::vec3( 2.0f,  5.0f, -15.0f), 
-  glm::vec3(-1.5f, -2.2f, -2.5f),  
-  glm::vec3(-3.8f, -2.0f, -12.3f),  
-  glm::vec3( 2.4f, -0.4f, -3.5f),  
-  glm::vec3(-1.7f,  3.0f, -7.5f),  
-  glm::vec3( 1.3f, -2.0f, -2.5f),  
-  glm::vec3( 1.5f,  2.0f, -2.5f), 
-  glm::vec3( 1.5f,  0.2f, -1.5f), 
-  glm::vec3(-1.3f,  1.0f, -1.5f)  
-};
+        glm::vec3( 0.0f,  0.0f,  0.0f), 
+        glm::vec3( 2.0f,  5.0f, -15.0f), 
+        glm::vec3(-1.5f, -2.2f, -2.5f),  
+        glm::vec3(-3.8f, -2.0f, -12.3f),  
+        glm::vec3( 2.4f, -0.4f, -3.5f),  
+        glm::vec3(-1.7f,  3.0f, -7.5f),  
+        glm::vec3( 1.3f, -2.0f, -2.5f),  
+        glm::vec3( 1.5f,  2.0f, -2.5f), 
+        glm::vec3( 1.5f,  0.2f, -1.5f), 
+        glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
+
+    // std::vector<glm::vec3> w123123;
+    // w123123.push_back(glm::vec3(-1.5f, 0.0f, -0.48f));
+    // w123123.insert(glm::vec3(-1.5f, 0.0f, -0.48f));
+    // = {
+    //     glm::vec3(-1.5f, 0.0f, -0.48f),
+    //     glm::vec3( 1.5f, 0.0f, 0.51f),
+    //     glm::vec3( 0.0f, 0.0f, 0.7f),
+    //     glm::vec3(-0.3f, 0.0f, -2.3f),
+    //     glm::vec3( 0.5f, 0.0f, -0.6f)
+    // };
+    glm::vec3 windows[] = {
+        // glm::vec3( 3.0f,  4.0f, -5.5f), 
+        glm::vec3(-1.5f, 0.0f, -0.48f),
+        glm::vec3( 1.5f, 0.0f, 0.51f),
+        glm::vec3( 0.0f, 0.0f, 0.7f),
+        glm::vec3(-0.3f, 0.0f, -2.3f),
+        glm::vec3( 0.5f, 0.0f, -0.6f)
+    };
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -206,22 +250,38 @@ int main(){
         // ------
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+        
+        justDraw(&ourShader,&shader2,pointLightPositions,cubePositions,VAO,lightVAO);
+        
+        shader2.use();
+        glBindVertexArray(transparentVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, inttexture_kuang);
+        // glBindTexture(GL_TEXTURE_2D, texture1);
 
-        glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);  
-        
-        glStencilFunc(GL_ALWAYS, 1, 0xFF); //全通过模板缓冲
-        glStencilMask(0xFF); //启动模板缓冲
-        glEnable(GL_STENCIL_TEST);
-        
-        justDraw(&ourShader,&lampShader,pointLightPositions,cubePositions,VAO,lightVAO);
-        
-        glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
-        glStencilMask(0x00);
-        glDisable(GL_DEPTH_TEST);
-        
-        justDraw(&lampShader,&lampShader,pointLightPositions,cubePositions,VAO,lightVAO,1.1f);
-        glStencilMask(0xFF);
-        glEnable(GL_DEPTH_TEST);  
+        std::map<float, glm::vec3> sorted;
+
+        for (unsigned int i = 0; i < 5; i++)
+        {
+            float distance = glm::length(camera.Position - windows[i]);
+            sorted[distance] = windows[i];
+        }
+
+        glm::mat4 view = camera.GetViewMatrix();
+       shader2.setMat4("view", view);
+
+       glm::mat4 projection = glm::mat4(1.0f);
+       projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+       // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+       shader2.setMat4("projection", projection);
+
+        for (std::map<float, glm::vec3>::reverse_iterator it = sorted.rbegin(); it != sorted.rend(); ++it)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, it->second);
+            shader2.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+        }
        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
        // -------------------------------------------------------------------------------
        glfwSwapBuffers(window);
@@ -248,7 +308,7 @@ int main(){
        ourShader->setVec3("viewPos", camera.Position);
 
         ourShader->setVec3("material.ambient",  1.0f, 0.5f, 0.31f);
-        ourShader->setVec3("material.diffuse",  1.0f, 0.5f, 0.31f);
+        // ourShader->setVec3("material.diffuse",  1.0f, 0.5f, 0.31f);
         ourShader->setFloat("material.shininess", 32.0f);
         //平行光
         ourShader->setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
--- opengl/shaders/shader.fs
+++ opengl/shaders/shader.fs
@@ -74,7 +74,7 @@ void main()
       result += CalcPointLight(pointLights[i],norm, FragPos ,viewDir);
    }
    result += CalcSpotLight(spotLight,norm,FragPos,viewDir);
-   FragColor = vec4(result, 1.0);
+   FragColor = vec4(result, 1.0f);
 }
 
 vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir){

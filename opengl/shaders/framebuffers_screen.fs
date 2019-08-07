47462815395e76e52a96ead2df49a2616d6a7fbd
zhumingwei <zdf312192599@163.com>
2019-08-07 18:54:01 +0800

    帧缓冲核

--- opengl/shaders/framebuffers_screen.fs
+++ opengl/shaders/framebuffers_screen.fs
@@ -1,4 +1,5 @@
 #version 330 core
+const float offset = 1.0 / 300.0;  
 out vec4 FragColor;
 
 in vec2 TexCoords;
@@ -7,17 +8,44 @@ uniform sampler2D screenTexture;
 
 void main()
 {
-    
-    // FragColor = texture(screenTexture, TexCoords);
-    // FragColor = vec4(1 - texture(screenTexture, TexCoords).rgb,1.0f);//反向
+     vec2 offsets[9] = vec2[](
+        vec2(-offset,  offset), // 左上
+        vec2( 0.0f,    offset), // 正上
+        vec2( offset,  offset), // 右上
+        vec2(-offset,  0.0f),   // 左
+        vec2( 0.0f,    0.0f),   // 中
+        vec2( offset,  0.0f),   // 右
+        vec2(-offset, -offset), // 左下
+        vec2( 0.0f,   -offset), // 正下
+        vec2( offset, -offset)  // 右下
+    );
+    //锐化
+    // float kernel[9] = float[](
+    //     -1, -1, -1,
+    //     -1,  9, -1,
+    //     -1, -1, -1
+    // );
+    //模糊
+    // float kernel[9] = float[](
+    //     1.0 / 16, 2.0 / 16, 1.0 / 16,
+    //     2.0 / 16, 4.0 / 16, 2.0 / 16,
+    //     1.0 / 16, 2.0 / 16, 1.0 / 16  
+    // );
+    //边缘检测
+    float kernel[9] = float[](
+            1, 1, 1,
+            1, -8 ,1,
+            1, 1, 1
+    );
 
-    //灰度
-    // FragColor = texture(screenTexture, TexCoords);
-    // float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
-    // FragColor = vec4(average, average, average, 1.0);
+    vec3 sampleTex[9];
+    for(int i = 0; i < 9; i++)
+    {
+        sampleTex[i] = vec3(texture(screenTexture, TexCoords.st + offsets[i]));
+    }
+    vec3 col = vec3(0.0);
+    for(int i = 0; i < 9; i++)
+        col += sampleTex[i] * kernel[i];
 
-    //灰度2
-    // FragColor = texture(screenTexture, TexCoords);
-    // float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
-    // FragColor = vec4(average, average, average, 1.0);
+    FragColor = vec4(col, 1.0);
 } 

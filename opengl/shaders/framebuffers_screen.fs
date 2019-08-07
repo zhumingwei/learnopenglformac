563c5c9abf8f48b4d0aafe45adc9e635cc97a583
zhumingwei <zdf312192599@163.com>
2019-08-07 16:39:12 +0800

    帧缓冲特效

--- opengl/shaders/framebuffers_screen.fs
+++ opengl/shaders/framebuffers_screen.fs
@@ -7,6 +7,17 @@ uniform sampler2D screenTexture;
 
 void main()
 {
-    vec3 col = texture(screenTexture, TexCoords).rgb;
-    FragColor = vec4(col, 1.0);
+    
+    // FragColor = texture(screenTexture, TexCoords);
+    // FragColor = vec4(1 - texture(screenTexture, TexCoords).rgb,1.0f);//反向
+
+    //灰度
+    // FragColor = texture(screenTexture, TexCoords);
+    // float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
+    // FragColor = vec4(average, average, average, 1.0);
+
+    //灰度2
+    // FragColor = texture(screenTexture, TexCoords);
+    // float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
+    // FragColor = vec4(average, average, average, 1.0);
 } 
